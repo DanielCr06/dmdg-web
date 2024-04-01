911949284977d495edd388d0882849d4073faa45
DanielCr06 <154911885+DanielCr06@users.noreply.github.com>
2024-04-01 21:20:57 +0200

    Add files via upload

---
+++ js/new.js
@@ -0,0 +1,7 @@
+const txt1 = document.getElementById('floatingText')
+const btn1 = document.getElementById('btn1')
+const out1 = document.getElementById('output')
+
+function fun1() {
+    out1.innerHTML = txt1.value;
+}
---
+++ js/script.js
@@ -0,0 +1,5 @@
+function returnText(){
+    let input = 
+document.getElementById('userInput').value
+  alert(input)
+}
